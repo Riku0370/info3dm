f4a919faf3bd7bbd32f8c541798d8e62f049085f
Riku Tamaki <e245734@ie.u-ryukyu.ac.jp>
2026-04-28 10:40:02 +0900

    演習1.1作成

---
+++ week_3/report/dataset1.py
@@ -0,0 +1,6 @@
+# フォント設定
+import numpy as np
+
+def true_function(x):
+    y = np.sin(np.pi * x * 0.8) * 10
+    return y
---
+++ week_3/report/run.py
@@ -0,0 +1,21 @@
+import numpy as np
+import dataset1
+# フォント設定
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as font_manager
+font_path = '/Library/Fonts/Arial Unicode.ttf'
+font_prop = font_manager.FontProperties(fname = font_path)
+plt.rcParams['font.family'] = font_prop.get_name()
+
+
+x = np.array([0])
+y = dataset1.true_function(x)
+print(y)
+
+x = np.linspace(-1, 1, 100)
+y = dataset1.true_function(x)
+
+plt.plot(x, y, label="true_function")
+plt.legend()
+plt.savefig("ex1.1.png")
+plt.show()
